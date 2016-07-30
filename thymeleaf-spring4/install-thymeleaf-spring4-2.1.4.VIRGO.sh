3eacf80f92b68813e2c00783c1e77a185c030546
fluffi <fluffi@datenkollektiv.de>
2016-07-30 14:07:07 +0200

    Redirect install to use Jenkins maven repository

--- thymeleaf-spring4/install-thymeleaf-spring4-2.1.4.VIRGO.sh
+++ thymeleaf-spring4/install-thymeleaf-spring4-2.1.4.VIRGO.sh
@@ -3,4 +3,6 @@ export GROUP_ID="org.thymeleaf"
 export VERSION="2.1.4.VIRGO"
 
 mvn clean compile bundle:bundle
-mvn install:install-file -Dfile=target/${ARTIFACT_ID}-${VERSION}.jar -DgroupId=${GROUP_ID} -DartifactId=${ARTIFACT_ID} -Dversion=${VERSION} -Dpackaging=jar
+
+# NOTE: Remove local directory path if not running on Jenkins
+mvn install:install-file -Dfile=target/${ARTIFACT_ID}-${VERSION}.jar -DgroupId=${GROUP_ID} -DartifactId=${ARTIFACT_ID} -Dversion=${VERSION} -Dpackaging=jar -DlocalRepositoryPath=/var/maven/repository/
