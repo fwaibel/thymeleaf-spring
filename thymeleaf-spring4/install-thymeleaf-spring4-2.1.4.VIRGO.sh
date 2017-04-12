b346296a63389c3a4d3881d4172dc332b78015b3
Florian Waibel <fwaibel@eclipsesource.com>
2017-04-12 23:47:30 +0200

    Add maven wrapper

--- thymeleaf-spring4/install-thymeleaf-spring4-2.1.4.VIRGO.sh
+++ thymeleaf-spring4/install-thymeleaf-spring4-2.1.4.VIRGO.sh
@@ -1,8 +1,33 @@
 export ARTIFACT_ID="thymeleaf-spring4"
 export GROUP_ID="org.thymeleaf"
 export VERSION="2.1.4.VIRGO"
+export BUILD_TYPE=CI
 
-mvn clean compile bundle:bundle
+USAGE_MESSAGE="Usage: $0 [-h] [-l]
+ -h help
+ -l local build"
 
-# NOTE: Remove local directory path if not running on Jenkins
-mvn install:install-file -Dfile=target/${ARTIFACT_ID}-${VERSION}.jar -DgroupId=${GROUP_ID} -DartifactId=${ARTIFACT_ID} -Dversion=${VERSION} -Dpackaging=jar -DlocalRepositoryPath=/var/maven/repository/
+while getopts hl args
+do case "$args" in
+  l) echo "switching to local build"
+     BUILD_TYPE="LOCAL";;
+  h) echo "${USAGE_MESSAGE}"
+     exit 1;;
+  :) echo "${USAGE_MESSAGE}"
+     exit 1;;
+  *) echo "${USAGE_MESSAGE}"
+     exit 1;;
+esac
+done
+
+shift $(($OPTIND - 1))
+
+./mvnw clean compile bundle:bundle
+
+if [ ${BUILD_TYPE} == "LOCAL" ]; then
+  echo "local build detected..."
+  ./mvnw install:install-file -Dfile=target/${ARTIFACT_ID}-${VERSION}.jar -DgroupId=${GROUP_ID} -DartifactId=${ARTIFACT_ID} -Dversion=${VERSION} -Dpackaging=jar
+else
+  echo "CI build detected..."
+  ./mvnw install:install-file -Dfile=target/${ARTIFACT_ID}-${VERSION}.jar -DgroupId=${GROUP_ID} -DartifactId=${ARTIFACT_ID} -Dversion=${VERSION} -Dpackaging=jar -DlocalRepositoryPath=/var/maven/repository/
+fi
